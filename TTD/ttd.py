26d860f69039d2ad5c782bee46f4048ef5cac751
Paul John <rainson.work@gmail.com>
2023-04-02 20:43:14 +0300

    Add sorting feature

--- TTD/ttd.py
+++ TTD/ttd.py
@@ -21,7 +21,7 @@ class Task(object):
         """Marks task as completed [x]"""
         self.completed = True
 
-    def __str__(self):
+    def __repr__(self):
         """string presentantion of tasks"""
         status = "[x]" if self.completed else "[ ]"
         return (f"{status} {self.description} \t| Due: {self.due_date}")
@@ -31,24 +31,33 @@ class TTD(object):
     """Terminal ToDo List Class"""
 
     def __init__(self, task=None):
-        """Initializes a TTD List
+        """
+        Initializes a TTD List
         Args: task - task object
         """
 
         self.tasks = []
 
-    def add_task(self, task):
-        """Adds a task to ToDo List"""
-        self.tasks.append(task)
+    def add_task(self, task, priority=0):
+        """
+        Adds a task to ToDo List
+        """
+        self.tasks.append({'Description': task, 'Priority': priority})
+        self.tasks = sorted(self.tasks, key=lambda x: x['Priority'])
 
     def remove_task(self, task):
-        """Removes a task from ToDo List"""
+        """
+        Removes a task from ToDo List
+        """
         self.tasks.remove(task)
 
     def print_tasks(self):
-        """Prints all the current Tasks in the ToDo List"""
+        """Prints sorted tasks"""
         for task in self.tasks:
-            print(task)
+            print(f"{task['Description'].description:<25} | {task['Description'].due_date:<15} | Priority: {task['Priority']:>1}")
+
+    def __repr__(self):
+        return '\n'.join(str(task) for task in self.tasks)
 
 
 if __name__ == "__main__":
