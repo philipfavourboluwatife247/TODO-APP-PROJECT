139a019cd3f9190eff8ada4c86d3e21828097c67
Paul John <rainson.work@gmail.com>
2023-04-02 15:49:42 +0300

    Implementing TTD

--- TTD/main.py
+++ TTD/main.py
@@ -5,7 +5,28 @@
 
 from sys import argv
 # import terminal todo app module
-import ttd
+from ttd import Task, TTD
 
 
+if __name__ == "__main__":
 
+    task1 = Task("This is my first task", "Today 6am")
+    task2 = Task("This is my second task", "Today 9am")
+    task3 = Task("This is my last task", "Today Allday")
+
+    # create new Terminal ToDo List
+    todo_list = TTD()
+
+    todo_list.add_task(task1)
+    todo_list.add_task(task2)
+    todo_list.add_task(task3)
+
+    todo_list.print_tasks()
+    print()
+
+    task1.mark_completed()
+
+    todo_list.print_tasks()
+    print()
+    todo_list.remove_task(task1)
+    todo_list.print_tasks()
