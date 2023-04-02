3547a77642c7b3fe27f3e8fe71cb3bb721a7c72a
Paul John <rainson.work@gmail.com>
2023-04-02 13:10:56 +0300

    Add todo module

---
+++ TODO/todo.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# todo.py by Paul John
+"""Terminal ToDo Application module"""
+
+class Todo(object):
+    pass
+
+
+if __name__ == "__main__":
+    print("This is only to be used as a module for main.py script.")
