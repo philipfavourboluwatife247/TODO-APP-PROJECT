3ed811ab665de8dbad96b1e0d5a815aef2a76c5b
Paul John <rainson.work@gmail.com>
2023-04-02 15:56:16 +0300

    Add comments in code blocks

--- TTD/main.py
+++ TTD/main.py
@@ -10,23 +10,28 @@ from ttd import Task, TTD
 
 if __name__ == "__main__":
 
+    # Create tasks
     task1 = Task("This is my first task", "Today 6am")
     task2 = Task("This is my second task", "Today 9am")
     task3 = Task("This is my last task", "Today Allday")
 
-    # create new Terminal ToDo List
+    # Create new Terminal ToDo List
     todo_list = TTD()
 
+    # Add tasks in ToDo List
     todo_list.add_task(task1)
     todo_list.add_task(task2)
     todo_list.add_task(task3)
 
+    # print all current tasks
     todo_list.print_tasks()
     print()
 
+    # Mark task as completed [x]
     task1.mark_completed()
-
     todo_list.print_tasks()
     print()
+
+    # Remove tasks from ToDo List
     todo_list.remove_task(task1)
     todo_list.print_tasks()
